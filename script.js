11ab660560a21e3c4f2dcd44a1f44e89d24b6726
Igor Mukhin <igor.mukhin@gmail.com>
2015-08-17 04:34:33 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+walk(document.body);
+
+function walk(node)  
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+	
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1: 
+		case 9:  
+		case 11: 
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling; 
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: 
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode) 
+{
+	var v = textNode.nodeValue;
+
+	v = v.replace(/Путине/g, "Хуйле");
+	v = v.replace(/Путиным/g, "Хуйлом");
+	v = v.replace(/Путину/g, "Хуйлу");
+	v = v.replace(/Путина/g, "Хуйла");
+	v = v.replace(/Путин/g, "Хуйло");
+	
+	textNode.nodeValue = v;
+}
